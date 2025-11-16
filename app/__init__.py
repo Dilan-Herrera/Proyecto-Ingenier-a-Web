e0333ec1824f45e24f434076a9f4077b353f7a3a
Dilan-Herrera <dilan.herrera@udla.edu.ec>
2025-11-15 21:54:55 -0500

    Add thE files for deploy

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,21 @@
 from flask import Flask
 from flask_pymongo import PyMongo
+import os
 
 mongo = PyMongo()
 
 
 def create_app():
     app = Flask(__name__)
-    app.config["MONGO_URI"] = "mongodb+srv://Dilan:UDLA@clusteringweb.lyhmd0l.mongodb.net/techadvisor_db?retryWrites=true&w=majority&appName=ClusterIngWEB"
 
-    app.secret_key = "clave_secreta"
+    mongo_uri = os.environ.get("MONGO_URI")
+
+    if not mongo_uri:
+        mongo_uri = "mongodb://localhost:27017/techadvisor_db"
+
+    app.config["MONGO_URI"] = mongo_uri
+
+    app.secret_key = os.environ.get("SECRET_KEY", "clave_dev_super_insegura")
 
     mongo.init_app(app)
 
